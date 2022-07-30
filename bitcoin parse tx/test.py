497a3f8d8bc5028c429fad36a793aedc296c4565
Feng XD <104902686+MesTR1c@users.noreply.github.com>
2022-07-30 20:10:47 +0800

    Delete bitcoin parse tx directory

--- bitcoin parse tx/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-url="https://api.blockcypher.com/v1/btc/test3/txs/736cd2c1b5cc3c25e51124048874f975af5ead09d7ee3ddaef56ea01fae73d4b"
-response = requests.get(url)
-print(response.text)
-with open("C://Users//86180//Desktop//bitcoin parse tx//result.txt","w") as f:
-    print("writing!!!")
-    f.write(response.text)
